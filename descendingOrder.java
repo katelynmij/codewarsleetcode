6215a6d0d5b6c46047f1066d2a5f9752717c446c
Katelyn Mijares <katelynmijares@gmail.com>
2025-09-26 14:59:07 -0500

    more easy ones

--- descendingOrder.java
+++ descendingOrder.java
@@ -1,21 +1,23 @@
 public class DescendingOrder {
 	public static int sortDesc(final int num) {
-		//sort
-		//min is 0
 		String numStr = String.valueOf(num);
 		int[] digits = new int[numStr.length()];
 		
-		
+		// array made of int digits	
 		for (int i = 0; i < numStr.length(); i++) {
 			digits[i] = Character.getNumericValue(numStr.charAt(i));
 		}
 
-		int min = digits[0];
-		for (int i = 1; i < digits.length(); i++) {
-			if (min > digits[i]) {
-				min = digits[i];
-			}
+		// sorts new array
+		Arrays.sort(digits);
+
+		// descending order
+		StringBuilder finalInt = new StringBuilder();
+		for (int i = digits.length - 1; i >= 0; i--) {
+			finalInt.append(digits[i]);
 		}
+
+		//convert string to integer
+		return Integer.parseInt(finalInt.toString());
 	}
 }
-//finish later
--- findTheOddInt.java
+++ findTheOddInt.java
@@ -1,5 +1,9 @@
 public class FindOdd {
     public static int findIt(int[] a) {
-
+        int result = 0;
+        for (int i = 0; i < a.length; i++) {
+            result ^= a[i];
+        }
+        return result;
     }
 }
