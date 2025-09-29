5386e5aff17d94324e64291a581d79e6b40e5b5c
Katelyn Mijares <katelynmijares@gmail.com>
2025-09-28 22:56:38 -0500

    lol

--- containerMostWater.java
+++ containerMostWater.java
@@ -1,14 +1,17 @@
 public class Solution {
     public Integer max_area(int[] heights) {
-        int left = 0, right = nums.length - 1;
-        int max;
+        int left = 0, right = heights.length - 1;
+        int max = 0;
 
         while (left < right) {
             int height = Math.min(heights[left], heights[right]);
             int width = right - left;
             int area = height * width;
 
-            max = Math.max(max, area);
+            //max = Math.max(max, area);
+            if (area > max){
+                max = area;
+            } 
 
             if (heights[left] < heights[right]) {
                 left++;
