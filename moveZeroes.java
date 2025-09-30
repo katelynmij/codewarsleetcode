273cd26bf315fa2875091211c14f33039bf8bf6e
Katelyn Mijares <katelynmijares@gmail.com>
2025-09-30 15:55:32 -0500

    two pointers

--- moveZeroes.java
+++ moveZeroes.java
@@ -1,5 +1,26 @@
 public class Solution {
     public int[] moveZeroes(int[] nums) {
-        // do later
+        // base case
+        int n = nums.length();
+        if (n < 2) {
+            return new int[] {0};
+        }
+        // pointers
+        int left = 0;
+        int right = 1;
+        // moving zeroes
+        while (right < n) {
+            if (nums[left] != 0) { // if left pointer is not zero, 
+                left++; // left keeps going and
+                right++; // right keeps going
+            } else if (nums[right] == 0) { // if right pointer is zero, we've found a zero
+                right++; // keep moving so left pointer gets detected as zero
+            } else { // left pointer found as zero
+                int tempHolder = nums[right]; // temporarily hold right value
+                nums[right] = nums[left]; // right is now left, the zero
+                nums[left] = tempHolder; // left is right, the non zero number
+            }
+        }
+        return nums;
     }
 }
--- twoSum.java
+++ twoSum.java
@@ -1,14 +1,12 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         int left = 0, right = nums.length - 1;
-        int[] solution = [2];
 
         while (left < right) {
             int currentSum = nums[left] + nums[right];
             if (currentSum == target) {
                 return new int[]{nums[left], nums[right]};
-            }
-            if (currentSum < target) {
+            } else if (currentSum < target) {
                 left++;
             } else {
                 right--;
